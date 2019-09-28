b2969bd02990c82d8f92657510797e9152a2e6fe
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-09-28 23:44:02 +0200

    Improve thread examples

--- basics/src/fundamentals/threads.rs
+++ basics/src/fundamentals/threads.rs
@@ -1,6 +1,16 @@
+use std::sync::mpsc;
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
+
+pub fn main() {
+    simple_example();
+    move_example();
+    builder_example();
+    simple_channel_example();
+    channel_multiple_messages();
+    channel_multiple_producers_and_messages();
+}
 
 fn simple_example() {
     let handle = thread::spawn(|| {
@@ -14,6 +24,16 @@ fn simple_example() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join();
+}
+
+fn move_example() {
+    let value = 5;
+    // move keyword force ownership to move
+    let handle = thread::spawn(move || {
+        println!("Value: {} ", value);
+    });
 
     handle.join();
 }
@@ -43,7 +63,77 @@ fn builder_example() {
     }
 }
 
-pub fn main() {
-    simple_example();
-    builder_example();
+pub fn simple_channel_example() {
+    // mpsc stands for multiple producer, single consumer
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap(); // ownership moved into thread
+    });
+
+    // recv() will wait until message received
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+pub fn channel_multiple_messages() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+
+    // rx can be used as an iterator
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+pub fn channel_multiple_producers_and_messages() {
+    let (tx, rx) = mpsc::channel();
+    let tx2 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+
+    // rx can be used as an iterator
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
